a1fe80b00444b6c8d8af149c5e82137c312fee22
Tanechka <truefirefox@gmail.com>
2017-07-09 22:16:27 -0700

    Lesson07 enum

---
+++ src/ru/javawebinar/basejava/TestSingleton.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.basejava;
+
+import ru.javawebinar.basejava.model.SectionType;
+
+public class TestSingleton {
+    private static TestSingleton instance;
+
+    public static TestSingleton getInstance() {
+        if (instance == null) {
+            instance = new TestSingleton();
+        }
+        return instance;
+    }
+
+    private TestSingleton() {
+    }
+
+    public static void main(String[] args) {
+        TestSingleton.getInstance().toString();
+        Singleton instance = Singleton.valueOf("INSTANCE");
+        System.out.println(instance.ordinal());
+
+        for (SectionType type : SectionType.values()) {
+            System.out.println(type.getTitle());
+        }
+    }
+
+    public enum Singleton {
+        INSTANCE
+    }
+}
---
+++ src/ru/javawebinar/basejava/model/SectionType.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.basejava.model;
+
+public enum SectionType {
+    PERSONAL("Личные качества"),
+    OBJECTIVE("Позиция"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
+    EXPERIENCE("Опыт работы"),
+    EDUCATION("Образование");
+
+    private String title;
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
+
