c4011b8d0a07aa0cbc07731809b6b0de61cd767b
Tanechka <truefirefox@gmail.com>
2017-07-14 13:18:43 -0700

    Lesson08 File and Resources

---
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -0,0 +1,37 @@
+package ru.javawebinar.basejava;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * gkislin
+ * 21.07.2016
+ */
+public class MainFile {
+    public static void main(String[] args) {
+        String filePath = ".\\.gitignore";
+
+        File file = new File(filePath);
+        try {
+            System.out.println(file.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Error", e);
+        }
+
+        File dir = new File("./src/ru/javawebinar/basejava");
+        System.out.println(dir.isDirectory());
+        String[] list = dir.list();
+        if (list != null) {
+            for (String name : list) {
+                System.out.println(name);
+            }
+        }
+
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
