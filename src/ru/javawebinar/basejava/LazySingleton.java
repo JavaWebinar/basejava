6a626f61722af844663860d1af284c53a9adf423
Tanechka <truefirefox@gmail.com>
2017-08-21 22:13:31 -0700

    Lesson11

---
+++ src/ru/javawebinar/basejava/LazySingleton.java
@@ -0,0 +1,24 @@
+package ru.javawebinar.basejava;
+
+public class LazySingleton {
+    volatile private static LazySingleton INSTANCE;
+
+    private LazySingleton() {
+    }
+
+    private static class LazySingletonHolder {
+        private static final LazySingleton INSTANCE = new LazySingleton();
+    }
+
+    public static LazySingleton getInstance() {
+        return LazySingletonHolder.INSTANCE;
+//        if (INSTANCE == null) {
+//            synchronized (LazySingleton.class) {
+//                if (INSTANCE == null) {
+//                    INSTANCE = new LazySingleton();
+//                }
+//            }
+//        }
+//        return INSTANCE;
+    }
+}
---
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -0,0 +1,72 @@
+package ru.javawebinar.basejava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainConcurrency {
+    public static final int THREADS_NUMBER = 10000;
+    private int counter;
+    private static final Object LOCK = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+
+        Thread thread0 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + ", " + getState());
+                throw new IllegalStateException();
+            }
+        };
+        thread0.start();
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+            }
+
+            private void inc() {
+                synchronized (this) {
+//                    counter++;
+                }
+            }
+
+        }).start();
+
+        System.out.println(thread0.getState());
+
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+                }
+            });
+            thread.start();
+            threads.add(thread);
+        }
+
+        threads.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println(mainConcurrency.counter);
+    }
+
+    private synchronized void inc() {
+//        synchronized (this) {
+//        synchronized (MainConcurrency.class) {
+        counter++;
+//                wait();
+//                readFile
+//                ...
+//        }
+    }
+}
